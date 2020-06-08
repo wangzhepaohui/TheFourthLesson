14dbfa5d633d2bb1ed72f1364ff5b61f5d0037bc
liuhaozhe <liuhaozhe@outlook.com>
2020-06-08 20:41:11 +0800

    8:41

--- flask-try1.py
+++ flask-try1.py
@@ -1,11 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello() -> str:
-    return 'hello word'
-
-
-app.run()
+from flask import Flaskapp = Flask(__name__)@app.route('/')def hello() -> str:    return 'hello word'app.run()
